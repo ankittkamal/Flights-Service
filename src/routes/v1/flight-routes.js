7160a4176453303c045405f2e8806044ec7cc488
ankittkamal <officiallyankit999@gmail.com>
2023-09-07 14:50:41 +0530

    Added getFlight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -11,6 +11,10 @@ router.post(
   FlightMiddlewares.validateCreateRequest,
   FlightController.createFlight
 );
+
 // /api/v1/flights?trips=MUM-DEL GET
 router.get("/", FlightController.getAllFlights);
+
+// /api/v1/flights:id GET
+router.get("/:id", FlightController.getFlight);
 module.exports = router;
